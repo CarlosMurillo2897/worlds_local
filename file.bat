0da96066ea7f89b2d5c5a1ed3fb3184ef61afe90
Carlos Murillo <cmb2897@gmail.com>
2024-11-07 00:56:50 -0600

    Auto-Commit at 07/11/2024- 0:56:26

--- file.bat
+++ file.bat
@@ -1,28 +1,49 @@
 @ECHO OFF
 CLS
 
-SET Game_ID=70600
-@REM SET Game_ID=892970
-SET Date_Time "%Date:~5%-%Time:~0,8%"
+@ECHO 0. Define variables.
+SET counter=0
+SET /a timeout=20
+SET /a limit=60
+SET Process_ID=892970
 SET Steam_PATH="C:\Program Files (x86)\Steam\Steam.exe"
-SET processName="Spotify.exe"
+SET Process_Name="valheim.exe"
+CALL :defineDateTime
 
-:waitForProcess
-tasklist /FI "IMAGENAME eq Spotify.exe" | find /I "Spotify.exe" >nul
-if %ERRORLEVEL% == 0 (
-    rem Process is running, wait a bit and check again
-    timeout /t 5 /nobreak >nul
-    goto waitForProcess
-)
+ECHO 1. Open Process %Process_Name% at %Date_Time%.
+%Steam_PATH% steam://rungameid/%Process_ID%
 
-ECHO Pull latest changes.
+ECHO 2. Fetch and pull from git folder.
 GIT fetch
 GIT pull
 
-ECHO Open Game.
-%Steam_PATH% steam://rungameid/%Game_ID%
+ECHO 3. Wait until process is closed.
+GOTO waitForProcess
 
-ECHO Push latest changes.
-@REM GIT add .
-@REM GIT commit -m "Commit "
-@REM GIT push
+:waitForProcess
+    tasklist /FI "IMAGENAME eq %Process_Name%" | find /I %Process_Name% >nul
+
+    if %ERRORLEVEL% == 0 (
+        TIMEOUT /t %timeout% /nobreak >nul
+
+        if %counter% EQU %limit% (
+            CALL :defineDateTime
+            CALL :createCommit
+            SET /a counter=0
+        ) else (
+            SET /a counter+=1
+        )
+        GOTO waitForProcess
+    )
+    CALL :createCommit
+    GOTO :EOF
+
+:createCommit
+    ECHO Commiting at %Date_Time%.
+    REM TODO: ONLY IF THERE'S SMTH AT GIT STATUS.
+    GIT add .
+    GIT commit -m "Auto-Commit at %Date_Time%"
+    GIT push
+
+:defineDateTime
+    SET Date_Time=%Date:~5%-%Time:~0,8%
